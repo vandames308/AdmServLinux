c1a214aebc51b26725e6b8dcc10cc881579fe51b
vandames308 <90427568+vandames308@users.noreply.github.com>
2021-09-23 20:14:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+sudo apt update -y &&
+echo "------ update realizado com sucesso :) ------"
+sudo apt list --upgradable &&
+sudo apt upgrade -y &&
+echo "------ upgrade realizado com sucesso :) ------"
